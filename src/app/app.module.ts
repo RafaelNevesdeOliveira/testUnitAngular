b5b62125d3056132775aef0b62126c05286ab640
RafaelNevesdeOliveira <rafaelneves652@gmail.com>
2022-07-31 00:47:04 -0300

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LikeWidgetComponent } from './shared/components/like-widget/like-widget.component';
+import { UniqueIdService } from './shared/services/unique-id/unique-id.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LikeWidgetModule
   ],
-  providers: [],
+  providers: [UniqueIdService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
