6d90352f9a9e345a4a40bea8dc269c59edd23fb9
Rafael Neves <rnoliveira@band.com.br>
2022-08-03 23:16:25 -0300

    up tests component

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -12,9 +12,8 @@ describe(LikeWidgetComponent.name, () => {
   // aguarda compilacao do componente para executar esse teste de mesa
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports:[LikeWidgetModule]
-    })
-    .compileComponents();
+      imports: [LikeWidgetModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -29,5 +28,21 @@ describe(LikeWidgetComponent.name, () => {
 
   it('#gritta should return true when called', () => {
     expect(component.gritta()).toBeTruthy();
-  })
+  });
+
+  it('Should auto-generate ID during ngOnInit when (@Input id) is not assigned', () => {
+    expect(component.id).toBeTruthy();
+  });
+
+  it('Should NOT auto-generate ID during ngOnInit when (@Input id) is not assigned', () => {
+    const someId = 'someId';
+    component.id = someId;
+    expect(component.id).toBe(someId);
+  });
+
+  it(`#${LikeWidgetComponent.prototype.like.name} should trigger (@Output liked) emission when called`, () => {
+    spyOn(component.liked, 'emit');
+    component.like();
+    expect(component.liked.emit).toHaveBeenCalled();
+  });
 });
